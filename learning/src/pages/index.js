d63f78b757eadf765113651ebc746b6cd603848c
Nhan Phan <phannhan.dn@gmail.com>
2021-02-28 22:09:28 +0700

    web learning

---
+++ learning/src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from '../Components/Layout'
+
+// https://www.youtube.com/playlist?list=PL4cUxeGkcC9hw1g77I35ZivVLe8k2nvjB
+
+export default function Home() {
+  return (
+    <Layout>
+      <section>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Da Nang</p>
+        </div>
+      </section>
+    </Layout>
+  )
+}
---
+++ learning/src/pages/projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+export default function Projects() {
+    return (
+        <Layout>
+            <div>
+                <h2>Portfolio</h2>
+                <h3>Projects & Website I've Created</h3>
+            </div>
+        </Layout>
+    )
+}

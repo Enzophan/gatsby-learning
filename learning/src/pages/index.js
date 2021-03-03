70ae784ffeebee13644823c4b0f89dd7fc76438c
Nhan Phan <phannhan.dn@gmail.com>
2021-03-03 23:52:17 +0700

    Update learning

--- learning/src/pages/index.js
+++ learning/src/pages/index.js
@@ -1,16 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from '../Components/Layout'
+import styles from '../Styles/home.module.css'
 
 // https://www.youtube.com/playlist?list=PL4cUxeGkcC9hw1g77I35ZivVLe8k2nvjB
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Da Nang</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
         </div>
       </section>
     </Layout>
--- learning/src/pages/projects/index.js
+++ learning/src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../../Components/Layout'
+import styles from '../../Styles/projects.module.css'
 
 export default function Projects() {
     return (
         <Layout>
-            <div>
+            <div className={styles.portfolio}>
                 <h2>Portfolio</h2>
                 <h3>Projects & Website I've Created</h3>
             </div>
